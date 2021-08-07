57ae63fc07301070c690efbea78018e0ddb31e95
Yubaraj Biswas <72277425+ybtheflash@users.noreply.github.com>
2021-08-07 20:07:15 +0530

    Update shwayy.js

--- scripts/shwayy.js
+++ scripts/shwayy.js
@@ -3,16 +3,9 @@ var s1 = new Audio('audio/shbaa.mp3');
 function playAudio() {
   s1.play();
 }
-function()
-{
-  if( window.localStorage )
-  {
-    if( !localStorage.getItem('firstLoad') )
-    {
-      localStorage['firstLoad'] = true;
-      window.location.reload();
-    }  
-    else
-      localStorage.removeItem('firstLoad');
-  }
+window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
 }

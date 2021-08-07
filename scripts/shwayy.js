09c47f1a013cce2c71d92a9476cec3fa9fc57821
Yubaraj Biswas <72277425+ybtheflash@users.noreply.github.com>
2021-08-07 20:04:35 +0530

    Update shwayy.js

--- scripts/shwayy.js
+++ scripts/shwayy.js
@@ -3,3 +3,16 @@ var s1 = new Audio('audio/shbaa.mp3');
 function playAudio() {
   s1.play();
 }
+function()
+{
+  if( window.localStorage )
+  {
+    if( !localStorage.getItem('firstLoad') )
+    {
+      localStorage['firstLoad'] = true;
+      window.location.reload();
+    }  
+    else
+      localStorage.removeItem('firstLoad');
+  }
+}
